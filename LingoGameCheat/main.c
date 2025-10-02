1429e1ad69237ec7f051267842d79c9419c13c44
sirusiru1818 <seosiru18@gmail.com>
2025-10-02 10:24:07 +0900

    Initial Commit

---
+++ LingoGameCheat/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LingoGameCheat
+//
+//  Created by U4cn on 10/2/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
